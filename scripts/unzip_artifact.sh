1214986c8056a544ff2b753e26ed00e3d0c05bc6
arakotoarivao <aina.rakotoarivao@gmail.com>
2025-03-07 15:03:58 +0100

    fix appspec

--- scripts/unzip_artifact.sh
+++ scripts/unzip_artifact.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-ls -l
-unzip -o artifact.zip -d /var/www/aws-chat
-
-chown -R nginx:nginx /var/www/aws-chat
-chmod -R 755 /var/www/aws-chat
-
 sudo systemctl stop nginx
+
+folder="/var/www/aws-chat/"
+ls -l
+ls -l $folder
+
+latest_deployment=$(ls -t /opt/codedeploy-agent/deployment-root/ | head -n 1)
+
+cd /opt/codedeploy-agent/deployment-root/$latest_deployment/ || exit 1
+
+ls -l
+
+if [ -f artifact.zip ]; then
+  echo "Unzipping artifact.zip..."
+  sudo unzip -o artifact.zip -d /opt/codedeploy-agent/deployment-root/$latest_deployment/ || exit 1
+else
+  echo "artifact.zip not found!"
+  exit 1
+fi
+
+echo "Deploying files..."
+sudo cp -r /opt/codedeploy-agent/deployment-root/$latest_deployment/app/dist/app/* $folder
+
+chown -R nginx:nginx $folder
+chmod -R 755 $folder
