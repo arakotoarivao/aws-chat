9e45c5f70201284854dee785af6353339c166ccb
arakotoarivao <aina.rakotoarivao@gmail.com>
2025-03-07 15:36:53 +0100

    try

--- scripts/unzip_artifact.sh
+++ scripts/unzip_artifact.sh
@@ -2,25 +2,26 @@
 sudo systemctl stop nginx
 
 folder="/var/www/aws-chat/"
-latest_deployment=$(ls -t /opt/codedeploy-agent/deployment-root/ | head -n 1)
+ongoing_deployment=$(ls -t /opt/codedeploy-agent/deployment-root/ | head -n 1)
 
-cd /opt/codedeploy-agent/deployment-root/$latest_deployment/ || exit 1
+cd /opt/codedeploy-agent/deployment-root/$ongoing_deployment/ || exit 1
 
-echo "Current directory: $(pwd)"
-echo "Current files:"
+latest_deployment=$(ls -t $pwd | head -n 1)
 
-ls -R
+cd $latest_deployment || exit 1
+
+ls -l
 
 if [ -f artifact.zip ]; then
   echo "Unzipping artifact.zip..."
-  sudo unzip -o artifact.zip -d /opt/codedeploy-agent/deployment-root/$latest_deployment/ || exit 1
+  sudo unzip -o artifact.zip -d /opt/codedeploy-agent/deployment-root/$ongoing_deployment/ || exit 1
 else
   echo "artifact.zip not found!"
   exit 1
 fi
 
 echo "Deploying files..."
-sudo cp -r /opt/codedeploy-agent/deployment-root/$latest_deployment/app/dist/app/* $folder
+sudo cp -r /opt/codedeploy-agent/deployment-root/$ongoing_deployment/app/dist/app/* $folder
 
 chown -R nginx:nginx $folder
 chmod -R 755 $folder
